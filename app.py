a569fce5566d20dc411007323a74ced068b2ca20
Cyriac <carl25.4917@gmail.com>
2025-11-11 23:35:18 +0100

    Initial commit du chat Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+# Importation des modules nécessaires
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+# Création de l'application Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'  # Clé secrète pour sécuriser les sessions
+
+# Initialisation de SocketIO pour gérer les messages en temps réel
+socketio = SocketIO(app)
+
+# Route principale : quand on va sur http://localhost:5000/
+@app.route('/')
+def index():
+    return render_template('chat.html')  # Affiche la page HTML du chat
+
+# Fonction qui gère les messages reçus
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message reçu :', msg)  # Affiche le message dans le terminal
+    send(msg, broadcast=True)     # Envoie le message à tous les clients connectés
+
+# Démarrage de l'application
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
