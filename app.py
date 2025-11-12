f9d448a811b1c1739a219a31e82dbf321cb50460
Cyriac <carl25.4917@gmail.com>
2025-11-12 14:44:36 +0100

    Fix WebSocket et compteur utilisateurs

--- app.py
+++ app.py
@@ -2,12 +2,26 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
+users_online = 0
+
 # Création de l'application Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'  # Clé secrète pour sécuriser les sessions
 
 # Initialisation de SocketIO pour gérer les messages en temps réel
 socketio = SocketIO(app)
+
+@socketio.on('connect')
+def handle_connect():
+    global users_online
+    users_online += 1
+    socketio.emit('user_count', users_online)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    global users_online
+    users_online -= 1
+    socketio.emit('user_count', users_online)
 
 # Route principale : quand on va sur http://localhost:5000/
 @app.route('/')
@@ -22,4 +36,4 @@ def handleMessage(msg):
 
 # Démarrage de l'application
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=10000)
